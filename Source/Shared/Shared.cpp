97925db8f863c09c0b74ad559443b0dc14ac0ba0
yokljo <yokljo@hotmail.com>
2026-06-19 11:25:58 +1000

    Fixed building on Linux. The build would fail because X11 defines Window, and it was taking precedence over nri::Window

--- Source/Shared/Shared.cpp
+++ Source/Shared/Shared.cpp
@@ -2,6 +2,11 @@
 
 #include "SharedExternal.h"
 
+#if NRI_ENABLE_VK_SUPPORT
+// Pull in the X11 "Window" typedef before "using namesapce nri" so it stays unambiguous with nri::Window.
+#include <vulkan/vulkan.h>
+#endif
+
 #include "HelperInterface.h"
 #include "ImguiInterface.h"
 #include "StreamerInterface.h"
